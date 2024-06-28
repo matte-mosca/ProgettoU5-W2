086bb28b24986e4e5ded5e7d098ac2f7211a7803
Matteo Moscardini <160152042+matte-mosca@users.noreply.github.com>
2024-06-28 11:41:43 +0200

    application started

---
+++ Progetto-U5-W2/src/main/java/matteomoscardini/progettou5w2/ProgettoU5W2Application.java
@@ -0,0 +1,13 @@
+package matteomoscardini.progettou5w2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5W2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoU5W2Application.class, args);
+    }
+
+}
---
+++ Progetto-U5-W2/src/test/java/matteomoscardini/progettou5w2/ProgettoU5W2ApplicationTests.java
@@ -0,0 +1,13 @@
+package matteomoscardini.progettou5w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU5W2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
